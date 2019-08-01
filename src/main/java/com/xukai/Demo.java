31ce70d5c5720bd10d9064a965c5258f0022b6d8
xukai <763545227@qq.com>
2019-08-01 10:02:04 +0800

    第一次上传

---
+++ src/main/java/com/xukai/Demo.java
@@ -0,0 +1,32 @@
+package com.xukai;
+
+public class Demo {
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+
+}
+
